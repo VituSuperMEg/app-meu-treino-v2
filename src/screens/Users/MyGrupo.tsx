5531700b0207aa14623e27f52b73531c3783e05a
VituSuperMEg <vitorpires8555@gmail.com>
2024-03-03 16:36:12 -0300

    Payment para criar novos grupos

--- src/screens/Users/MyGrupo.tsx
+++ src/screens/Users/MyGrupo.tsx
@@ -5,6 +5,9 @@ import { Scroll } from "phosphor-react-native";
 export function MyGrupo() {
 
   const grupo = false;
+
+  const payment = true;
+
   return (
     <Box flex={1}>
       {!grupo && (
@@ -12,6 +15,16 @@ export function MyGrupo() {
           <Scroll color="#858585"/>
           <Text variant="body" color="textBody">
             Não temos grupos cadastrados
+          </Text>
+          <Text variant="body" color="textBody" textAlign="center" marginTop="l" marginBottom="xl">
+            Caso tenha vontade de participar de um grupo
+            Entre em contato com Personal e utlizar a chave de acesso.
+            {"\n"}
+            {payment && (
+            <Text variant="bold" color="greenPrimary" onPress={() => navigate("Create")}>
+              Crie um grupo.
+            </Text>
+            )}
           </Text>
         </Box>
       )}
