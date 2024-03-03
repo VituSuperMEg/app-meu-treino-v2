662b89cbadf8b2eeaef7fee9407f09baf3442e01
VituSuperMEg <vitorpires8555@gmail.com>
2024-03-03 16:40:32 -0300

    Colocando Empty para as tabs do usuario

--- src/screens/Users/MyMarkerPosts.tsx
+++ src/screens/Users/MyMarkerPosts.tsx
@@ -1,12 +1,19 @@
 import { Box } from "@components/Box";
 import { Text } from "@components/Text";
+import { NotePencil } from "phosphor-react-native";
 
 export function MyMarkerPosts() {
+  const notPosts = false;
   return (
-    <Box>
-      <Text variant="body" color="shape">
-        Meus Marcados
-      </Text>
-    </Box>
+    <Box flex={1}>
+    {!notPosts && (
+      <Box padding="m" alignItems="center" justifyContent="center" flex={1} marginBottom="xl">
+        <NotePencil color="#858585"/>
+        <Text variant="body" color="textBody" padding="s" textAlign="center">
+          Ninguém ainda lhe marcou em nenhum post..
+        </Text>
+      </Box>
+    )}
+  </Box>
   )
 }
