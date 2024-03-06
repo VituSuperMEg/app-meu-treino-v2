71d599afe68b58e9eab813582929463ef7636338
VituSuperMEg <vitorpires8555@gmail.com>
2024-03-06 07:19:04 -0300

    Fazendo uma estilização básica

--- src/components/Select.tsx
+++ src/components/Select.tsx
@@ -1,22 +1,42 @@
 import RNPickerSelect from 'react-native-picker-select';
+import {Text} from './Text';
 
-
-export const Select = () => {
+interface ISelect {
+  label: string;
+  required?: boolean;
+  items: [{label: string; value: any}];
+  onValueChange: (value: any) => void;
+}
+export function Select({label, required, items, onValueChange}: ISelect) {
   return (
-    <RNPickerSelect
-      style={{
-        viewContainer : {
-          borderWidth : 1,
-          borderColor : '#858585',
-          borderRadius : 6,
-        },
-      }}
-      onValueChange={value => console.log(value)}
-      items={[
-        {label: 'Football', value: 'football'},
-        {label: 'Baseball', value: 'baseball'},
-        {label: 'Hockey', value: 'hockey'},
-      ]}
-    />
+    <>
+      <Text variant="body" color="shape">
+        {label}{' '}
+        {required && (
+          <Text variant="body" color="danger">
+            *
+          </Text>
+        )}
+      </Text>
+      <RNPickerSelect
+        placeholder={{
+          label : label,
+          color: '#858585',
+        }}        
+        pickerProps={{
+          dropdownIconColor: '#858585',
+          mode : 'dialog',
+        }}
+        style={{
+          viewContainer: {
+            borderWidth: 1,
+            borderColor: '#858585',
+            borderRadius: 6,
+          },
+        }}
+        onValueChange={onValueChange}
+        items={items}
+      />
+    </>
   );
-};
+}
