7a539834141138a60a5074fb0ed85d191c96a499
VituSuperMEg <vitorpires8555@gmail.com>
2024-03-07 20:46:20 -0300

    Finalização do Component RatingEmoji

--- src/components/RatingEmoji.tsx
+++ src/components/RatingEmoji.tsx
@@ -1,31 +1,47 @@
-import { TouchableOpacity } from "react-native";
-import { Box } from "./Box";
-import { Text } from "./Text";
+import {TouchableOpacity} from 'react-native';
+import {Box} from './Box';
+import {Text} from './Text';
+import {useState} from 'react';
 
 export function RatingEmoji() {
-
-  const emoji = ["🤩", "😁", "😀", "😔", "😭"];
-  
-
+  const emoji = ['🤩', '😁', '😀', '😔', '😭'];
+  const [emojiPress, setEmojiPress] = useState('');
+  const nameEmoji = {
+    '🤩': 'Incrível',
+    '😁': 'Legal',
+    '😀': 'Bom',
+    '😔': 'Ruim',
+    '😭': 'Péssimo',
+  };
   return (
-    <Box flexDirection="row" gap="l">
+    <Box flexDirection="row" gap="l" mb="m">
       {emoji.map((e, index) => (
-        <TouchableOpacity key={index} onPress={() => console.log(e)}>
+        <TouchableOpacity
+          key={index}
+          onPress={() => setEmojiPress(e)}
+          style={{
+            alignItems: 'center',
+          }}>
           <Box
-            backgroundColor="zinc"
+            backgroundColor={emojiPress === e ? 'greenPrimary' : 'zinc'}
             width={50}
             height={50}
             borderRadius={8}
             alignItems="center"
-            justifyContent="center"
-          >
-            <Text variant="body" fontSize={20}>
+            justifyContent="center">
+            <Text variant="body" fontSize={20} color="shape">
               {e}
             </Text>
           </Box>
+          <Box position="absolute" top={50}>
+            {emojiPress === e && (
+              <Text variant="bodyMin" color="textBody">
+                {nameEmoji[e]}
+              </Text>
+            )}
+          </Box>
         </TouchableOpacity>
-
       ))}
     </Box>
-  )
+  );
 }
