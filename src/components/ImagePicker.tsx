ef68f5b202f0a2becc21a1426f5e66ef28a1ae34
VituSuperMEg <vitorpires8555@gmail.com>
2024-03-09 12:25:37 -0300

    Criando mudar o component de imagem picker

--- src/components/ImagePicker.tsx
+++ src/components/ImagePicker.tsx
@@ -1,36 +1,31 @@
-import * as React from 'react';
-import {
-  Image,
-  Platform,
-  SafeAreaView,
-  ScrollView,
-  StyleSheet,
-  TouchableOpacity,
-  View,
-} from 'react-native';
+import {useCallback, useState} from 'react';
+import {Image, Platform, TouchableOpacity, View} from 'react-native';
 
 import * as ImagePicker from 'react-native-image-picker';
 import {Box} from './Box';
 import {Text} from './Text';
-import {Camera} from 'phosphor-react-native';
+import {Camera, Trash} from 'phosphor-react-native';
 
 const includeExtra = true;
 
 export function ImagePickerComponent({
   setImage,
   salvar,
+  profile = false
 }: {
   setImage: any;
   salvar: any;
+  profile: boolean;
 }) {
-  const [response, setResponse] = React.useState<any>(null);
+  const [response, setResponse] = useState<any>(null);
+  const [selectedImage, setSelectedImage] = useState(false);
 
-  const onButtonPress = React.useCallback((type, options) => {
+  const onButtonPress = useCallback((type, options) => {
     if (type === 'capture') {
       ImagePicker.launchCamera(options, response => {
         if (!response.didCancel) {
           setResponse(response);
-          setImage(response.assets); 
+          setImage(response.assets);
         }
       });
     } else {
@@ -45,37 +40,60 @@ export function ImagePickerComponent({
 
   return (
     <>
-    {!response?.assets && actions.map(({title, type, options}) => {
-      return (
-        <TouchableOpacity key={title} onPress={() => onButtonPress(type, options)}>
-          <Box
-            borderWidth={1}
-            borderColor="textBody"
-            height={200}
-            width={200}
-            borderRadius={200}
-            alignItems="center"
-            justifyContent="center"
-            gap="s">
-            <Camera color="#858585" />
-            <Text variant="body" color="textBody">
-              Clique aqui
-            </Text>
-          </Box>
-        </TouchableOpacity>
-      );
-    })}
-    {response?.assets && response?.assets.map(({ uri }: { uri: string }) => (
-      <View key={uri} style={{ margin: 5 }}>
-        <Image
-          style={{ width: 200, height: 200, objectFit: 'cover' }}
-          resizeMode="cover"
-          resizeMethod="scale"
-          source={{ uri: uri }}
-        />
-      </View>
-    ))}
-  </>
+      {!response?.assets &&
+        actions.map(({title, type, options}) => {
+          return (
+            <TouchableOpacity
+              key={title}
+              onPress={() => onButtonPress(type, options)}>
+              <Box
+                borderWidth={1}
+                borderColor="textBody"
+                height={200}
+                width={200}
+                borderRadius={200}
+                alignItems="center"
+                justifyContent="center"
+                gap="s">
+                <Camera color="#858585" />
+                <Text variant="body" color="textBody">
+                  Clique aqui
+                </Text>
+              </Box>
+            </TouchableOpacity>
+          );
+        })}
+      {response?.assets &&
+        response?.assets.map(({uri}: {uri: string}) => (
+          <TouchableOpacity
+            key={uri}
+            onPress={() => setSelectedImage(prev => !prev)}>
+            {selectedImage && (
+              <Box
+                position="absolute"
+                zIndex={999}
+                width={200}
+                height={200}
+                borderRadius={profile ? 200 : 0} 
+                backgroundColor="transparent"
+                alignItems="center"
+                justifyContent="center">
+                <TouchableOpacity onPress={() => setResponse(null)}>
+                  <Trash color="#ef4444" size={40} />
+                </TouchableOpacity>
+              </Box>
+            )}
+            <View>
+              <Image
+                style={{width: 200, height: 200, objectFit: 'cover', borderRadius : profile ? 200: 0}}
+                resizeMode="cover"
+                resizeMethod="scale"
+                source={{uri: uri}}
+              />
+            </View>
+          </TouchableOpacity>
+        ))}
+    </>
   );
 }
 
