a82a001c642645afaa262bfd27f340a71f019267
VituSuperMEg <vitorpires8555@gmail.com>
2024-03-11 13:20:07 -0300

    Alterado o compinent de header para se comportar diferente no modal

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -6,19 +6,22 @@ import { TouchableOpacity, View } from 'react-native';
 import { useNoticafion } from '../store/notification';
 import { useNavigation } from '@react-navigation/native';
 import { useState } from 'react';
+import { Notification } from './Notification';
 
 
 interface IHeader {
   style : 'home' | 'menu';
   name?: string;
   favorite? : boolean;
+  isModal : boolean;
+  setShow?: React.Dispatch<React.SetStateAction<boolean>>;
 }
-export function Header({ style = 'home', name = '', favorite = false }:IHeader) {
+export function Header({ style = 'home', name = '', favorite = false, setShow , isModal = false}:IHeader) {
   const [favorites, setFavorites] = useState("");
   const [fav, setFav] = useState(false);
   const user = useUser(state => state.user);
   const count = useNoticafion(state => state.count);
-  
+   
 
   const handleFavoriteTreino = (id : string) => {
     setFavorites(id)
@@ -39,30 +42,14 @@ export function Header({ style = 'home', name = '', favorite = false }:IHeader) 
               {user.name}
             </Text>
           </Text>
-          <View>
-            <TouchableOpacity>
-              <Barbell size={40} color="#5ED25C" />
-            </TouchableOpacity>
-            <View
-              style={{
-                backgroundColor: 'red',
-                width: 20,
-                height: 20,
-                position: 'absolute',
-                alignItems: 'center',
-                right: -5,
-                borderRadius: 10,
-              }}>
-              <Text variant="body" color="shape" fontSize={12}>
-                {count >= 100 ? 99 : count}
-              </Text>
-            </View>
-          </View>
+          <Notification />
         </Box>
       )}
       {style === 'menu' && (
         <Box padding="m" flexDirection="row" alignItems="center" gap="l" justifyContent={favorite ? 'space-between' : 'flex-start'}>
-          <TouchableOpacity onPress={() => goBack()}>
+          <TouchableOpacity onPress={() => {
+            isModal ? setShow(false) : goBack()
+          }}>
             <ArrowLeft color="#fff" />
           </TouchableOpacity>
           <Text variant="body" color="shape">
